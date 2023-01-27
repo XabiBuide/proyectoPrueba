7123207a5b056de02a7b61b8acf38705f3488bce
XabiBuide <123190455+XabiBuide@users.noreply.github.com>
2023-01-27 17:35:07 +0100

    Update Principal.java

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Arriba españa");
 		System.out.println("Est00pido");
+		System.out.println("SUUUUUUUUUUUUU");
 	}
 }
